a12ae0258498ae132fd6fd93f21f71cd169d62ec
codycodyco <70448201+codycodyco@users.noreply.github.com>
2023-12-20 02:09:53 +0800

    翻译

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -282,12 +282,12 @@ export default function TopNavigationBar({
                     )}
                   </Popover>
                 </Popover.Group> */}
-                <button
+                {/* <button
                   className="cursor-pointer inline-flex items-center px-1 border-b-2 border-transparent text-base font-medium leading-6 text-gray-500 hover:text-gray-900 hover:border-gray-300 focus:outline-none focus:text-gray-900 focus:border-gray-300 dark:text-dark-high-emphasis dark:hover:border-gray-500 dark:focus:border-gray-500 transition"
                   onClick={() => setIsContactUsActive(true)}
                 >
                   联系我们
-                </button>
+                </button> */}
               </div>
             </div>
             <div
@@ -303,7 +303,7 @@ export default function TopNavigationBar({
                   aria-hidden="true"
                 />
 
-                <span className="ml-2 font-medium">Search</span>
+                <span className="ml-2 font-medium">搜索</span>
               </button>
             </div>
             <div className="flex items-center lg:hidden">
